d3ec8020eb059dfe745b0068973d70339dddcfa7
Leonardo de Faveri <faver_i@hotmail.com>
2024-09-26 08:26:53 -0300

    initial config for app

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import Home from "@/screens/Home";
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+  return (
+    <>
+      <Home />
+      <StatusBar style="auto" />
+    </>
+  );
+}
