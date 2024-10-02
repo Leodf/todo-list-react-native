571628254e9e03038cc78a85457719ff654c276b
Leonardo de Faveri <faver_i@hotmail.com>
2024-10-02 12:09:34 -0300

    refactor App entry

--- App.tsx
+++ App.tsx
@@ -5,8 +5,6 @@ import {
   Inter_400Regular,
   Inter_700Bold,
 } from "@expo-google-fonts/inter";
-import { View, Text } from "react-native";
-import { fonts } from "@/constants/tokens";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
